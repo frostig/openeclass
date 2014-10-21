007d686d2467e2915d4390908a2031471b8833d3
frostig <lena-simfo@hotmail.com>
2014-10-21 21:02:05 +0300

    exercise edits

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-19 05:12
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'elena';
+$mysqlPassword = 'pepe';
+$mysqlMainDb = 'eclass';
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
